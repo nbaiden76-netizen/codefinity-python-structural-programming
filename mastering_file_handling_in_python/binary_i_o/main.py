28c5b0abd48c8ecbdb9c747fb9d23c60aff7cbe3
nbaiden76-netizen <>
2026-05-02 09:23:36 +0000

    Binary I/O

---
+++ mastering_file_handling_in_python/binary_i_o/main.py
@@ -0,0 +1,7 @@
+# Write Python code to open a file named "data.bin" in binary write mode and write the bytes b'PythonRocks!' to it.
+
+# Write your code here
+
+# Then, open the same file in binary read mode, read the contents, and store them in a variable named "read_bytes".
+
+# Write your code here
